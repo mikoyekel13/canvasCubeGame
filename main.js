cf30c11e5e2200e01af374a4e79ab08a47f23cb5
mikoyekel13 <mikoyekel@hilma.tech>
2023-10-24 11:04:27 +0300

    first

--- main.js
+++ main.js
@@ -9,17 +9,19 @@ function startGame() {
     myGameArea.start();
 }
 
-var myGameArea = {
-    canvas : document.createElement("canvas"),
-    start : function() {
+class myGameArea {
+    constructor() {
+        this.canvas = document.createElement("canvas");
+    }
+    static start() {
         this.canvas.width = 480;
         this.canvas.height = 270;
         this.context = this.canvas.getContext("2d");
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
         this.frameNo = 0;
         this.interval = setInterval(updateGameArea, 20);
-        },
-    clear : function() {
+        }
+    static clear() {
         this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
     }
 }
